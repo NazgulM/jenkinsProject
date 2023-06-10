5b6371335d4d82393f18661a3efb2eddfdeb9d8b
NazgulM <maniyasova.n@gmail.com>
2023-06-10 13:21:54 -0500

    groovy

---
+++ Projects/Jobs/2.groovy
@@ -0,0 +1,45 @@
+template = '''
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    run: terraform
+  name: terraform
+spec:
+  containers:
+  - command:
+    - sleep
+    - "3600"
+    image: hashicorp/terraform
+    name: terraform
+    '''
+
+
+tfvars = '''
+    ami_id = "ami-092b51d9008adea15"
+    instance_type = "t2.micro"
+    availability_zone = "us-east-2a"
+    key_name = "my_mini"
+'''
+
+podTemplate(cloud: 'kubernetes', label: 'terraform', yaml: template) {
+    node ('terraform') {
+        container('terraform') {
+            stage ('Clone repo') {
+        git branch: 'main', url: 'https://github.com/NazgulM/jenkins-terraform.git'
+    }
+
+    stage('Init') {
+        sh 'terraform init'
+    }
+
+    withCredentials([usernamePassword(credentialsId: 'aws-creds', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
+    stage ('terraform apply') {
+        writeFile file: 'hello.tfvars', text: tfvars
+        sh 'terraform apply -var-file hello.tfvars --auto-approve'
+    }
+}  
+        }
+}
+}
+
--- Projects/jenkins_master_node/README.md
+++ Projects/jenkins_master_node/README.md
@@ -4,3 +4,10 @@
 
 With Jenkins you can automate anything. In my case My Master and Agent are running on Digital Cloud. Configure Agent Node in Jenkins.
 
+SSH Connection (the better way)
+Step 1 — Set up Jenkins Master instance, install java 11 and jenkins
+
+Step 2 — Set up Jenkins Agent instance
+
+Then create a key pair for this user.
+
